660da3ea37afc138e6cbd0b9f58ffe173802dbb9
j3nmasr-tech <j3n.masr@gmail.com>
2025-11-15 20:28:39 +0200

    Add configuration for trading settings
    Added configuration settings for check interval, timeframes, and instruments.

---
+++ config.py
@@ -0,0 +1,17 @@
+CHECK_INTERVAL = 60
+
+TIMEFRAMES = [
+    "15m",
+    "30m",
+    "1h",
+    "4h"
+]
+
+INSTRUMENTS = [
+    "eurusd",
+    "gbpusd",
+    "audusd",
+    "nzdusd",
+    "usdjpy",
+    "usdchf"
+]
