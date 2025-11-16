af458ac42ed479a3e41e9685d29ea75b5292d51f
j3nmasr-tech <j3n.masr@gmail.com>
2025-11-16 18:08:25 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from config import CONFIG
-
-def load_config():
-    return CONFIG
-
-def log(msg: str):
-    ts = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{ts}] {msg}", flush=True)
