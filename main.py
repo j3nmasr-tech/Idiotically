dcec456f4a1b35832a2d3b7ce45a656008ccceb9
j3nmasr-tech <j3n.masr@gmail.com>
2025-12-20 21:20:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Config:
     
     # Scanning
     SCAN_INTERVAL = int(os.getenv("SCAN_INTERVAL", "10"))
-    TOP_N = int(os.getenv("TOP_N", "20"))
+    TOP_N = int(os.getenv("TOP_N", "30"))
     TIMEFRAMES = ["1m", "3m", "5m", "15m", "30m"]
     
     # Signal thresholds
